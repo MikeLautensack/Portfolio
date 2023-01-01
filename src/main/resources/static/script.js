92f8aebfae06d71f81f7b663a65ae4778524dfba
Mike <mikelautensack100@gmail.com>
2022-12-31 22:44:14 -0500

    more styling ... getting it ready to deploy if need to

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,5 +1,7 @@
 const nav = document.querySelector("nav ul");
 const navButton = document.querySelector(".nav-button");
+let launchButtons = document.querySelectorAll('.launch');
+let cards = document.querySelectorAll('.card');
 
 navButton.addEventListener("click", () => {
     const vis = nav.getAttribute("data-visable");
@@ -12,3 +14,19 @@ navButton.addEventListener("click", () => {
         navButton.setAttribute("aria-expanded", false);
     }
 });
+
+/*launchButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        console.log('1')
+        
+        cards.forEach(card => {
+            console.log('2')
+            let isCardLive = card.getAttribute('data-is-project-live');
+            if(isCardLive == 'false') {
+                card.setAttribute('data-display-coming-soon', 'true');
+                console.log('3')
+            }
+        });
+    });
+});*/
+
