852b40fef51ca1b6575721a3f0dbf08a371c309c
Mike <mikelautensack100@gmail.com>
2023-12-21 02:04:40 -0500

    Reran the build commands and created the routing and component skeleton

---
+++ portfolio/src/components/Header.tsx
@@ -0,0 +1,15 @@
+import Nav from "./Nav";
+
+export default function Header() {
+  return (
+    <header>
+        <div
+            id="logo-container"
+            className=""
+        >
+
+        </div>
+        <Nav />
+    </header>
+  )
+}
