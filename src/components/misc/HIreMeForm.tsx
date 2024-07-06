ecfd7c5c25a6484e9226ee7da7753e4a459b6ad1
Mike <mikelautensack100@gmail.com>
2024-07-06 18:08:33 -0400

    updated env vars

--- src/components/misc/HIreMeForm.tsx
+++ src/components/misc/HIreMeForm.tsx
@@ -35,7 +35,7 @@ const HireMeForm = () => {
     formData: HireMeFormValues
   ) => {
     console.log(formData);
-    const res = await fetch(`${process.env.NEXT_PUBLIC_HOST}/api/email`, {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_HOST}api/email`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
