c6b79a92c606ba13a86df5d356ace59d9314389d
MikeLautensack <mikelautensack100@gmail.com>
2024-07-27 15:34:32 -0400

    build error fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ const Header = () => {
       component="header"
       className="w-full h-14 flex justify-center items-center bg-[#001824] sticky top-0 z-20"
     >
-      <Box comonent="div" className="absolute left-0 lg:ml-32 flex gap-2">
+      <Box component="div" className="absolute left-0 lg:ml-32 flex gap-2">
         <Typography variant="h5" color="#FF8D25">
           Mike
         </Typography>
